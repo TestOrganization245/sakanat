d7ea74b713c56151e68168caa96070ab09d47921
ibtisamkharrosheh <ibtisamkharrosheh@gmail.com>
2023-08-03 21:28:18 +0300

    hh

--- src/main/java/najah/edu/application/LOGIN.java
+++ src/main/java/najah/edu/application/LOGIN.java
@@ -19,6 +19,7 @@ public class LOGIN {
     public static String ten = "";
     
     
+    
     public void run() {
         while (true) {
            System.out.println("Welcome to the Application!");
