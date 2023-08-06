76c7d632f61ff1b9b36dc146459245a1265f9e17
ibtisamkharrosheh <ibtisamkharrosheh@gmail.com>
2023-08-07 01:10:22 +0300

    owner

--- src/main/java/najah/edu/application/OWNER.java
+++ src/main/java/najah/edu/application/OWNER.java
@@ -17,81 +17,8 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import najah.edu.application.apartment;
 
 
-/*
- class apartment {
-    private int id;
-    private static int nextId = 1;
-    private int floorId;
-    private int numBathrooms;
-    private int numBedrooms;
-    private boolean hasBalcony;
-    private boolean reserved;
-    private int idres;
-
-    
-    public apartment( int floorId, int bath, int bed, boolean y) {
-    	  this.id = nextId++;
-        this.floorId = floorId;
-        this.numBathrooms = bath;
-        this.numBedrooms = bed;
-        this.hasBalcony = y;
-      
-    } 
-    public apartment(int id, int floorId, int bath, int bed, boolean y) {
-        this.floorId = floorId;
-        this.numBathrooms = bath;
-        this.numBedrooms = bed;
-        this.hasBalcony = y;
-        this.id = nextId++;
-    }
-    
-    public apartment(int id, int floorId, int bath, int bed, boolean y, int idd ) {
-        this.floorId = floorId;
-        this.numBathrooms = bath;
-        this.numBedrooms = bed;
-        this.hasBalcony = y;
-        this.id = nextId++;
-        this.idres = idd;
-    }
-
-    public int getId() {
-        return id;
-    }
-    
-    public boolean getRR() {
-        return reserved;
-    }
-
-    public int getFloorId() {
-        return floorId;
-    }
-
-    public int getNumBathrooms() {
-        return numBathrooms;
-    }
-
-    public int getNumBedrooms() {
-        return numBedrooms;
-    }
-
-    public boolean hasBalcony() {
-        return hasBalcony;
-    }
-    
-    
-    public static void resetIdCounter() {
-        nextId = 1;
-    }
-    public int getres() {
-        return idres;
-    }
-
-}
-
-*/
 class Residence {
     private static int nextId = 1;
 
@@ -716,7 +643,7 @@ public class OWNER {
 		            }
 
 		            int currentResidenceId = Integer.parseInt(dataLines[0].split(": ")[1]);
-		           // ibtisam = currentResidenceId;
+		            
 		            if (currentResidenceId == residenceId) {
 		                int apartmentId = Integer.parseInt(dataLines[1].split(": ")[1]);
 		                int floorId = Integer.parseInt(dataLines[2].split(": ")[1]);
@@ -781,7 +708,7 @@ public class OWNER {
 	        return result.toString();
 	    }
 	}
-	public List<apartment> aa(List<apartment> a) {
+	public static List<apartment> aa(List<apartment> a) {
 		newapp= a;
 		return newapp;
 	}
